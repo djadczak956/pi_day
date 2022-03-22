a4545d8a1418482c9364bf98442f7aedf3081759
Damian J <jadczakdamian@gmail.com>
2022-03-22 13:02:01 -0400

    Made the introductory text for the program

--- number_reader.py
+++ number_reader.py
@@ -1,3 +1,12 @@
+# This program compares the digits of pi and e up to a length that the user specifies
+# This program also plots the distribution of digits
+
+# Prints out the introductory text
+def intro():
+    print("Hello! This program will compare the digits of the two irrational numbers:")
+    print("pi (3.14...) and e (2.71...). You will also be given the option to compare")
+    print("the distribution of digits with a bar graph. Enjoy.\n\n")
+
 # Reads as many digits of the file as you specify
 def read_file(file_name, length):
     with open(file_name) as file:
@@ -7,22 +16,19 @@ def read_file(file_name, length):
 # Asks for how many digits of a number that you will analyze
 def get_length():
     length = int(input("Enter how many digits you want to analyze (from 1 to 1 million): "))
-
     while length <= 0 or length > 1_000_000:
         print("Invalid number. Please try again!")
         length = int(input("Enter how many digits you want to analyze (from 1 to 1 million): "))
-
     return length
 
 def assign_digits(num_file, length):
-    numbers = [0 for i in range(10)]
+    numbers = [0] * 10  # Create an array from index 0-9
     for digit in num_file:
         numbers[int(digit)] += 1
     print(numbers)
 
-# TODO: Turn dictionary into array great
-# TODO: Use a bar plot to show the distribution
+# TODO: Use a bar plot to compare the 2 irrational numbers
 # ---------------- MAIN ------------------
+intro()
 length = get_length()
 assign_digits(read_file("e.txt", length), length)
-
