511180b352fe31de132d9bf3b74e9bb041848e31
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2022-03-24 12:59:30 -0400

    Almost done with graph

--- number_reader.py
+++ number_reader.py
@@ -1,6 +1,10 @@
 # This program compares the digits of pi and e up to a length that the user specifies
 # This program also plots the distribution of digits
 
+# Imports
+import matplotlib.pyplot as plt
+import numpy as np
+
 # Prints out the introductory text
 def intro():
     print("Hello! This program will compare the digits of the two irrational numbers:")
@@ -29,10 +33,23 @@ def assign_digits(num_file, name, length):
     for i in range(10):
         print(str(i) + ": " + str(numbers[i]))
     print()
+    return numbers
+
+# https://www.geeksforgeeks.org/bar-plot-in-matplotlib/
+def create_graph(pi_list, e_list):
+    width = 0.25
+    fig = plt.subplots(figsize=(12, 8))
+    ax = fig.add_axes([0, 0, 1, 1])
+
+    bar1 = np.arange(len(pi_list))  # The x shift to get multiple bars
+    bar2 = [i + width for i in bar1]    # Also x shift, for the second bar
+
+    
+
 
 # TODO: Use a bar plot to compare the 2 irrational numbers
 # ---------------- MAIN ------------------
 intro()
 length = get_length()
-assign_digits(read_file("pi.txt", length), "pi", length)
-assign_digits(read_file("e.txt", length), "e", length)
+pi = assign_digits(read_file("pi.txt", length), "pi", length)   # Prints and assigns pi digits
+e = assign_digits(read_file("e.txt", length), "e", length)      # Prints and assigns e digits
