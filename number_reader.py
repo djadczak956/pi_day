7193a3affda26423615e1133da410e5221ff4cd3
Damian Jadczak <62266703+djadczak956@users.noreply.github.com>
2022-03-25 12:46:48 -0400

    Probably done

--- number_reader.py
+++ number_reader.py
@@ -1,9 +1,9 @@
 # This program compares the digits of pi and e up to a length that the user specifies
-# This program also plots the distribution of digits
+# This program also plots the distribution of digits with a bar graph
 
 # Imports
-from venv import create
-import matplotlib.pyplot as plt
+from venv import create     # pipenv stuff
+import matplotlib.pyplot as plt     # Use for bar graph
 import numpy as np
 
 # Prints out the introductory text
@@ -13,34 +13,55 @@ def intro():
     print("the distribution of digits with a bar graph. Enjoy.\n\n")
 
 # Reads as many digits of the file as you specify
+# file_name refers to the name of a file
+# Returns a string of text of a specified length
 def read_file(file_name, length):
     with open(file_name) as file:
         pi_sample = file.read(length)
         return pi_sample   
 
 # Asks for how many digits of a number that you will analyze
+# Returns an integer 
 def get_length():
-    length = int(input("Enter how many digits you want to analyze from pi and e (from 1 to 1 million): "))
+    length = -1
+    
+    # While the value is invalid 
     while length <= 1 or length > 1_000_000:
-        print("Invalid number. Please try again (2 to 1 million)!")
-        length = int(input("Enter how many digits you want to analyze (from 1 to 1 million): "))
+
+        # Try and except to make sure input is valid
+        try:
+            length = int(input("Enter how many digits you want to analyze (from 2 to 1 million): "))
+        except ValueError:
+            print("Invalid number. Please try again (2 to 1 million)!")
+            continue
+
     return length
 
+# num_file refers to a string of digits that will have its digits read and counts split into an array
+# name refers to the name of the math constant
 def assign_digits(num_file, name, length):
     numbers = [0] * 10  # Create an array from index 0-9
+    
+    # Adds to count for the corresponding digit
+    # Uses an array and adds 1 to the corresponding index from 0-9
     for digit in num_file:
         numbers[int(digit)] += 1
-    print(name + ":")
+
+    print(name + ":") # Prints the name of the mathematical constant
+    
+    # Prints out the numbers and how many times they appear in the math constant
     for i in range(10):
         print(str(i) + ": " + str(numbers[i]))
-    print()
+
+    print()     # Spacing line
     return numbers
 
-# https://www.geeksforgeeks.org/bar-plot-in-matplotlib/
+# Uses matplotlib.pyplot to create a bar graph
+# Takes in a list of digits from pi and e that can be generated with assign_digits
+# Also takes in a length that can be acquired with the get_length method
 def create_graph(pi_list, e_list, length):
     bar_width = 0.2
     fig = plt.subplots(figsize=(12, 8))
-    #ax = fig.add_axes([0, 0, 1, 1])
 
     # Positioning for the bars
     bar1 = np.arange(len(pi_list))  # The x position of first bar
@@ -61,10 +82,9 @@ def create_graph(pi_list, e_list, length):
     plt.show()
 
 
-# TODO: Use a bar plot to compare the 2 irrational numbers
 # ---------------- MAIN ------------------
 intro()
 length = get_length()
 pi = assign_digits(read_file("pi.txt", length), "pi", length)   # Prints and assigns pi digits
 e = assign_digits(read_file("e.txt", length), "e", length)      # Prints and assigns e digits
-create_graph(pi, e, length)
+create_graph(pi, e, length)     # Creates a bar graph from the lists of digits
